7fdb6e2883409a8bc584a0870f6f94ba76837b42
kyrers <kyrers.eth@gmail.com>
2023-02-03 21:43:55 +0000

    Remove unnecessary data type

--- src/hooks/useLatestBlock.ts
+++ src/hooks/useLatestBlock.ts
@@ -1,15 +1,9 @@
 import { BigNumber } from "ethers";
 import useSWR from "swr";
 
-type Data = {
-    jsonrpc: string;
-    id: string;
-    result: string;
-};
-
 export default function useLatestBlock(chainId: any) {
     const fetcher = (...args: [any]) => fetch(...args).then((res) => res.json());
-    const { data, error, isLoading } = useSWR<Data>(`api/getLatestBlock?chainId=${chainId}`, fetcher, {
+    const { data, error, isLoading } = useSWR(`api/getLatestBlock?chainId=${chainId}`, fetcher, {
         revalidateIfStale: false,
         revalidateOnFocus: false,
         revalidateOnReconnect: false
