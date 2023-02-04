670f088449a1dc507cb6f3cb53809896ad7751b3
kyrers <kyrers.eth@gmail.com>
2023-02-04 19:45:46 +0000

    Determine contracts for which we must fetch the abi

--- src/hooks/useApproval.ts
+++ src/hooks/useApproval.ts
@@ -1,3 +1,5 @@
+import { ALL_APPROVAL_TOPICS } from "@/utils/constants";
+import { useEffect } from "react";
 import useSWR from "swr";
 
 export default function useApprovals(chainId: any, txHashList: any[] | undefined) {
@@ -8,7 +10,8 @@ export default function useApprovals(chainId: any, txHashList: any[] | undefined
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(args[0].hashList)
-            }).then((res) => res.json());
+            })
+            .then((res) => res.json());
         }
 
         return undefined;
@@ -19,6 +22,25 @@ export default function useApprovals(chainId: any, txHashList: any[] | undefined
         revalidateOnFocus: false,
         revalidateOnReconnect: false
     });
-    
+
+    useEffect(() => {
+        if (data) {
+            let filteredReceipts = data.filter((response: any) => response.result.logs.length > 0).map((filteredResponses: any) => filteredResponses.result);
+            getContractsABI(filteredReceipts);
+        }
+    }, [data]);
+
     return { erc20Approvals: data, isLoading, isError: error };
 };
+
+export const getContractsABI = (receipts: any[]) => {
+    let fetchedABIs : string[] = [];
+    receipts.forEach((receipt: any) => {
+        receipt.logs.forEach((log: any) => {
+            //Only need to get ABI of contracts not fetched yet and with events that correspond to approvals
+            if(!fetchedABIs.includes(log.address) && ALL_APPROVAL_TOPICS.includes(log.topics[0])) {
+                fetchedABIs.push(log.address);
+            }
+        })
+    });
+}
