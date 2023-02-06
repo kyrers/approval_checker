6f010ccf3cfa01ec1e426801b40822c788920b7f
kyrers <kyrers.eth@gmail.com>
2023-02-06 22:12:56 +0000

    useApprovals now has a mapping of contract address and logs it emitted

--- src/hooks/useApproval.ts
+++ src/hooks/useApproval.ts
@@ -6,7 +6,7 @@ export default function useApprovals(chainId: any, txHashList: any[] | undefined
     const receiptsFetcher = (...args: [any]) => {
         const hashList = args[0].hashList;
         if (hashList && hashList.length > 0) {
-            //POST REQUEST TO SEND TX HASH LIST. OTHERWISE IT WOULD BE SENT VIA URL WHICH IS INSANE
+            //POST request to send hashList. Otherwise it would be sent via url which is insane
             return fetch(args[0].url, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
@@ -28,7 +28,7 @@ export default function useApprovals(chainId: any, txHashList: any[] | undefined
     const abisFetcher = (...args: [any]) => {
         const contractAddresses = args[0].contractAddresses;
         if (contractAddresses.length > 0) {
-            //POST REQUEST TO SEND contractAddresses LIST. OTHERWISE IT WOULD BE SENT VIA URL WHICH IS INSANE
+            //POST request to send contractAddresses LIST. Otherwise it would be sent via url which is insane
             return fetch(args[0].url, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
@@ -41,28 +41,34 @@ export default function useApprovals(chainId: any, txHashList: any[] | undefined
         return undefined;
     };
 
-    const [contractAddresses, setContractAddresses] = useState<string[]>([]);
-    const { data: abis, error: abisError, isLoading: isLoadingABIs } = useSWR({ url: `api/getContractABI?chainId=${chainId}`, contractAddresses: contractAddresses }, abisFetcher, {
+    const [addressToLogsMap, setAddressToLogsMap] = useState<Map<string, any[]>>(new Map());
+    const { data: abis, error: abisError, isLoading: isLoadingABIs } = useSWR({ url: `api/getContractABI?chainId=${chainId}`, contractAddresses: Array.from(addressToLogsMap.keys()) }, abisFetcher, {
         revalidateIfStale: false,
         revalidateOnFocus: false,
         revalidateOnReconnect: false
     });
 
     const fetchContractsABIFromReceipts = async (receipts: any[]) => {
-        let abisToFetch: string[] = [];
+        //Map to hold the contract address and the total of logs it emitted
+        let _addressToLogsMap = new Map<string, any[]>();
+
         if (receipts) {
             let filteredReceipts = receipts.filter((response: any) => response.result.logs.length > 0).map((filteredResponses: any) => filteredResponses.result);
             filteredReceipts.forEach((receipt: any) => {
                 receipt.logs.forEach((log: any) => {
-                    //Only need to get ABI of contracts not fetched yet and with events that correspond to approvals
-                    if (!abisToFetch.includes(log.address) && ALL_APPROVAL_TOPICS.includes(log.topics[0])) {
-                        abisToFetch.push(log.address);
+                    //Get all events that correspond to approvals
+                    if (ALL_APPROVAL_TOPICS.includes(log.topics[0])) {
+                        if (_addressToLogsMap.get(log.address)) {
+                            _addressToLogsMap.get(log.address)!.push(log.topics);
+                        } else {
+                            _addressToLogsMap.set(log.address, [log.topics]);
+                        }
                     }
                 })
             });
         }
 
-        setContractAddresses(abisToFetch);
+        setAddressToLogsMap(_addressToLogsMap);
     };
 
     return { erc20Approvals: receipts, isLoadingReceipts, isError: receiptsError };
