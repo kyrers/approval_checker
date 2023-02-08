1cc8d2a1746556ef19faf8b0144207a1ce55bd8d
kyrers <kyrers.eth@gmail.com>
2023-02-08 23:01:19 +0000

    Decode Events
    Fixed bug where abis were refetched when any state var updated;
    Handled case where contracts emitted events not in their ABI. Not sure how this is possible.

--- src/hooks/useApproval.ts
+++ src/hooks/useApproval.ts
@@ -1,4 +1,4 @@
-import { ALL_APPROVAL_TOPICS } from "@/utils/constants";
+import { ALL_APPROVAL_TOPICS, FALLBACK_INTERFACE } from "@/utils/constants";
 import { getNetworkKey } from "@/utils/shared";
 import { Contract, ethers } from "ethers";
 import { useState } from "react";
@@ -9,6 +9,7 @@ type UserApprovalInfo = {
     contractABI: any;
     contract: Contract | undefined;
     logsEmitted: any[];
+    decodedEvents: any[];
 }
 
 export default function useApprovals(chainId: any, txHashList: any[] | undefined) {
@@ -35,8 +36,11 @@ export default function useApprovals(chainId: any, txHashList: any[] | undefined
     });
 
     const abisFetcher = (...args: [any]) => {
+        const isToFetch = args[0].isToFetch;
         const contractAddresses = args[0].contractAddresses;
-        if (contractAddresses.length > 0) {
+        if (isToFetch && contractAddresses && contractAddresses.length > 0) {
+            setIsToFetchABIs(false);
+
             //POST request to send contractAddresses LIST. Otherwise it would be sent via url which is insane
             return fetch(args[0].url, {
                 method: "POST",
@@ -44,23 +48,26 @@ export default function useApprovals(chainId: any, txHashList: any[] | undefined
                 body: JSON.stringify(contractAddresses)
             })
                 .then((res) => res.json())
-                .then((abis) => createContractInstances(abis));
+                .then((abis) => createContractInstancesAndDecodeEvents(abis));
         }
 
         return undefined;
     };
 
-    //const [addressToLogsMap, setAddressToLogsMap] = useState<Map<string, any[]>>(new Map());
-    const [userApprovalInfo, setUserApprovalInfo] = useState<UserApprovalInfo[]>([]);
+    const [contractAddressesToFetch, setContractAddressesToFetch] = useState<any[]>([]);
+    // This bool is needed to control when to fetch ABIs, otherwise when any other state variable updated it would try to fetch again, since the abi fetcher useSWR uses state vars as a parameter
+    const [isToFetchABIs, setIsToFetchABIs] = useState(false);
+    const [userApprovalInfo, setUserApprovalInfo] = useState<UserApprovalInfo[] | undefined>([]);
 
-    const { data: abis, error: abisError, isLoading: isLoadingABIs } = useSWR({ url: `api/getContractABI?chainId=${chainId}`, contractAddresses: userApprovalInfo.map(uai => uai.contractAddress) }, abisFetcher, {
+    const { data: abis, error: abisError, isLoading: isLoadingABIs } = useSWR({ url: `api/getContractABI?chainId=${chainId}`, contractAddresses: contractAddressesToFetch, isToFetch: isToFetchABIs }, abisFetcher, {
         revalidateIfStale: false,
         revalidateOnFocus: false,
         revalidateOnReconnect: false
     });
 
-    const fetchContractsABIFromReceipts = async (receipts: any[]) => {
+    const fetchContractsABIFromReceipts = (receipts: any[]) => {
         let _userApprovalInfo: UserApprovalInfo[] = [];
+        let abisToFetch: any[] = [];
 
         if (receipts) {
             let filteredReceipts = receipts.filter((response: any) => response.result.logs.length > 0).map((filteredResponses: any) => filteredResponses.result);
@@ -70,9 +77,10 @@ export default function useApprovals(chainId: any, txHashList: any[] | undefined
                     if (ALL_APPROVAL_TOPICS.includes(log.topics[0])) {
                         const existingObject = _userApprovalInfo.find(uai => uai.contractAddress === log.address);
                         if (existingObject) {
-                            existingObject.logsEmitted.push(log.topics);
+                            existingObject.logsEmitted.push({ data: log.data, topics: log.topics });
                         } else {
-                            _userApprovalInfo.push({ contractAddress: log.address, contractABI: undefined, contract: undefined, logsEmitted: [log.topics] })
+                            abisToFetch.push(log.address);
+                            _userApprovalInfo.push({ contractAddress: log.address, contractABI: undefined, contract: undefined, logsEmitted: [{ data: log.data, topics: log.topics }], decodedEvents: [] })
                         }
                     }
                 })
@@ -80,18 +88,34 @@ export default function useApprovals(chainId: any, txHashList: any[] | undefined
         }
 
         setUserApprovalInfo(_userApprovalInfo);
+        setContractAddressesToFetch(abisToFetch);
+        setIsToFetchABIs(true);
     };
 
-    const createContractInstances = async (abis: any[]) => {
+    const createContractInstancesAndDecodeEvents = (abis: any[]) => {
         let provider = new ethers.providers.AlchemyProvider(ethers.providers.getNetwork(chainId), getNetworkKey(chainId));
         let _userApprovalInfo = userApprovalInfo;
 
         if (abis) {
             abis.forEach((abi: any) => {
-                let existingObject = _userApprovalInfo.find((uai) => uai.contractAddress === abi.address);
+                let existingObject = _userApprovalInfo?.find(uai => uai.contractAddress === abi.address);
+
                 if (existingObject) {
                     existingObject.contractABI = abi.data.result;
                     existingObject.contract = new Contract(existingObject.contractAddress, existingObject.contractABI, provider);
+                    existingObject.logsEmitted.forEach((log: any) => {
+                        /**
+                        * Some contracts throwed an error when decoding the event because, somehow, their abi doesn't have the event emitted.
+                        * To handle those situations, we have a FALLBACK_INTERFACE that has all the events we want to decode
+                        */
+                        try {
+                            let decodedEvent = existingObject?.contract?.interface.parseLog({ topics: log.topics, data: log.data });
+                            existingObject?.decodedEvents.push(decodedEvent)
+                        } catch (_) {
+                            let decodedEvent = FALLBACK_INTERFACE.parseLog({ topics: log.topics, data: log.data });
+                            existingObject?.decodedEvents.push(decodedEvent);
+                        }
+                    });
                 }
             });
         }
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,12 @@
+import { ethers } from "ethers";
+
 export const ERC20_APPROVAL_TOPIC = "0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925";
 export const ERC721_APPROVAL_TOPIC = "0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925";
 export const ERC721_APPROVAL_FOR_ALL_TOPIC = "0x17307eab39ab6107e8899845ad3d59bd9653f200f220920489ca2b5937696c31";
 export const ERC1155_APPROVAL_FOR_ALL_TOPIC = "0x17307eab39ab6107e8899845ad3d59bd9653f200f220920489ca2b5937696c31";
 export const ALL_APPROVAL_TOPICS = [ERC20_APPROVAL_TOPIC, ERC721_APPROVAL_TOPIC, ERC721_APPROVAL_FOR_ALL_TOPIC, ERC1155_APPROVAL_FOR_ALL_TOPIC];
+export const FALLBACK_INTERFACE = new ethers.utils.Interface([
+    "event Transfer(address indexed from, address indexed to, uint256 value)",
+    "event Approval(address indexed owner, address indexed spender, uint256 value)",
+    "event ApprovalForAll(address indexed owner, address indexed operator, bool approved)"]
+);
