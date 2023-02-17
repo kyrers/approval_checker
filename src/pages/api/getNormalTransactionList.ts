bff21116ee4e4d6f0046ebf658c32bb54904db5f
kyrers <kyrers.eth@gmail.com>
2023-02-17 20:49:17 +0000

    Modified order to be from most to less recent

--- src/pages/api/getNormalTransactionList.ts
+++ src/pages/api/getNormalTransactionList.ts
@@ -5,7 +5,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     try {
         const correctAPIKey = getChainAPIKey(req.query.chainId);
         const partialURL = getAccountNormalTransactionListApiUrl(req.query.chainId);
-        const fullURL = `${partialURL}&address=${req.query.address}&startblock=0&endblock=${req.query.latestBlock}&offset=10000&sort=asc&apikey=${correctAPIKey}`;
+        const fullURL = `${partialURL}&address=${req.query.address}&startblock=0&endblock=${req.query.latestBlock}&offset=10000&sort=desc&apikey=${correctAPIKey}`;
         const data = await fetch(fullURL).then((response) => response.json());
         return res.status(200).json(data);
     } catch (err) {
